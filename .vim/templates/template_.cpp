1d50c1a903270a11c643654928b1e8feb4d4067b
Oliver Dain <oliver@revl.com>
2017-08-18 12:36:47 -0700

    Added Revl templates.

---
+++ .vim/templates/template_.cpp
@@ -0,0 +1,11 @@
+//
+// <+VIM_FILE,:t+>
+// Revl
+//
+// Created by Oliver Dain on <+DATE,%m/%d/%Y+>
+// Copyright © 2017 Revl. All rights reserved.
+//
+
+namespace XXX {
+
+}  // namespace
---
+++ .vim/templates/template_.h
@@ -0,0 +1,16 @@
+//
+// <+VIM_FILE,:t+>
+// Revl
+//
+// Created by Oliver Dain on <+DATE,%m/%d/%Y+>
+// Copyright © 2017 Revl. All rights reserved.
+//
+
+#ifndef <+INCLUDE_GUARD,.*cpp+>
+#define <+INCLUDE_GUARD,.*cpp+>
+
+namespace XXX {
+
+}  // namespace
+
+#endif  // <+INCLUDE_GUARD,.*cpp+>
